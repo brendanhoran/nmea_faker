49d2402d9838c4237281b53e4dad512d6eb04bb0
Brendan Horan <brendan@horan.hk>
2017-06-29 21:47:51 +0800

    fixing pep8 stlye issues

--- server/nmea_server.py
+++ server/nmea_server.py
@@ -4,7 +4,8 @@
 # Author  : Brendan Horan
 
 
-import pynmea2, time
+import pynmea2
+import time
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
 
@@ -14,8 +15,14 @@ api = Api(app, prefix="/api/v1")
 
 
 nmea_sentence_parser = reqparse.RequestParser()
-nmea_sentence_parser.add_argument("lat", type=float, required=True, help="Input latitude coordinates")
-nmea_sentence_parser.add_argument("lon", type=float, required=True, help="Input longitude coordinates")
+nmea_sentence_parser.add_argument(
+    "lat", type=float,
+    required=True,
+    help="Input latitude coordinates")
+nmea_sentence_parser.add_argument(
+    "lon",
+    type=float,
+    required=True, help="Input longitude coordinates")
 
 
 class server_info(Resource):
@@ -23,16 +30,20 @@ class server_info(Resource):
         info = "nmea_faker server"
         return(info, 200)
 
+
 class nmea_sentence(Resource):
     def put(self):
         coordinates = nmea_sentence_parser.parse_args(strict=True)
         utc_time = time.strftime("%H%M%S", time.gmtime())
         utc_date = time.strftime("%d%m%y", time.gmtime())
- 
-        nmea_raw = "$GPRMC,{0},A,{2},N,{3},E,0.168,,{1},,,A".format(utc_time, utc_date, coordinates.lat, coordinates.lon)
+        nmea_raw = "$GPRMC,{0},A,{2},N,{3},E,0.168,,{1},,,A".format(
+            utc_time,
+            utc_date,
+            coordinates.lat, coordinates.lon)
         nmea_gprmc = pynmea2.parse(nmea_raw)
         nmea_gprmc = str(nmea_gprmc)
         return(nmea_gprmc, 201)
+
 
 def run_server():
     api.add_resource(server_info, '/nmea_faker')
