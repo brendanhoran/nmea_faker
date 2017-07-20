0e6f8ec88ad33c9915eb2d023e0c849dd3d9807c
Brendan Horan <brendan@horan.hk>
2017-07-20 23:02:03 +0800

    nmea base functions

--- client/nmea_client.py
+++ client/nmea_client.py
@@ -1,7 +1,14 @@
 import urequests
+import time
 
-print("nmea client beginning")
+print("NMEA client beginning")
+print("REPL prompt will not spawn")
 
-sentence = urequests.put('http://nmea:5000/api/v1/nmea_faker/sentence',  json={'lat': '123','lon': '456'}, headers={'content-type': 'application/json'})
-print(sentence.text)
+task_start_time = time.time()
+while True:
+  sentence = urequests.put('http://nmea:5000/api/v1/nmea_faker/sentence',
+    json={'lat': '123','lon': '456'},
+    headers={'content-type': 'application/json'})
+  print(sentence.text)
+  time.sleep(60.0 - ((time.time() - task_start_time) % 60.0))
 
