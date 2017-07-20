cedb2c1324b34331626252facf8d72ccfe6c8130
Brendan Horan <brendan@horan.hk>
2017-07-20 22:19:38 +0800

    adding wifi module

--- client/wifi_setup.py
+++ client/wifi_setup.py
@@ -1,1 +1,16 @@
+import network
+import sys
+import time
+
 print("Wifi setup beginning")
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+wlan.connect("SSID", "PASSWORD")
+time.sleep(5)
+
+if wlan.isconnected() == False:
+  print("WiFi not connected")
+  wlan.disconnect()
+else:
+  print("Wifi connected")
+  print(wlan.ifconfig())
