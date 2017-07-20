aad8ed5343b5a1c345381a461a738c6409de4105
Brendan Horan <brendan@horan.hk>
2017-07-20 22:30:56 +0800

    adding nmea client

--- client/nmea_client.py
+++ client/nmea_client.py
@@ -1,1 +1,7 @@
+import urequests
+
 print("nmea client beginning")
+
+sentence = urequests.put('http://nmea:5000/api/v1/nmea_faker/sentence',  json={'lat': '123','lon': '456'}, headers={'content-type': 'application/json'})
+print(sentence.text)
+
