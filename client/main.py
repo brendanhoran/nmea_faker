d766dd36b90b1bb10bc74f37cd167c03ce5763a1
Brendan Horan <brendan@horan.hk>
2017-12-10 17:29:28 +0800

    main.py working

--- client/main.py
+++ client/main.py
@@ -1,2 +1,19 @@
+# Author Brendan Horan
+# License : BSD 3-Clause
+# Description : Get a NMEA setence from the NMEA faker sever
+
 import wifi_setup
 import nmea_client
+import time
+wifi_setup = wifi_setup.WIFI_setup('YOUR_SSID','YOUR_PASSWORD')
+wifi_setup.connect()
+
+# format is, RX pin, TX pin, IP address and port of server
+nmea_client = nmea_client.NMEA_client(25,26,'127.0.0.1:5000')
+
+time.sleep(10)
+print("Starting NMEA client, REPL will not spawn")
+task_start_time = time.time()
+while True:
+  nmea_client.get_sentence()
+  time.sleep(60.0 - ((time.time() - task_start_time) % 60.0))
